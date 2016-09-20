3b12bfee343012d95135ca4c52bbcfa73ff39c0d
angelless <dongkyu91622@gmail.com>
2016-09-20 17:39:09 +0900

    #2

--- src/AdminSay/Main.java
+++ src/AdminSay/Main.java
@@ -1,29 +1,31 @@
 package AdminSay
 
-public class Main extends PluginBase implements  Listener{
+public class Main extends PluginBase implements Listener {
 
-Set<Player> Sayer;
+	Set<Player> Sayer;
 
-@Override
-public void onEnable(){
-this.getServer().getPluginManager().registerEvents(this,this);
-this.Sayer = new HashSet<>();
-}
-@Override
-public boolean onCommand(CommandSender sender, Command command , String label, String[] args){
-if(command.getName().equals("say")){
- if(sender instanceof Player){
-   if(sender.isOp()){
-/*아직 sayer에 등록되지 않았을때*/
-if(){
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(this, this);
+		this.Sayer = new HashSet<>();
+	}
 
-/*이미 sayer에 등록되어있을때*/
-}else if(){
+	@Override
+	public boolean onCommand(CommandSender sender, Command command , String label, String[] args){
+		if(command.getName().equals("say")){
+			if(sender instanceof Player){
+				if(sender.isOp()){
+					/*아직 sayer에 등록되지 않았을때*/
+					if(){
 
-}
-}
-}
+	   				/*이미 sayer에 등록되어있을때*/
+					}else if(){
+					
+					}
+   				}
+ 			}
+		}
+
+	}
 }
 
-}
-}
